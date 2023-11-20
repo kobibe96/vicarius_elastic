44d69e005db61b240335a36ae54cf1a291064037
kobibe96 <79308704+kobibe96@users.noreply.github.com>
2023-11-20 10:38:11 +0200

    Update ElasticConfig.java

--- src/main/java/com/vicarius/vicarius/configuration/ElasticConfig.java
+++ src/main/java/com/vicarius/vicarius/configuration/ElasticConfig.java
@@ -3,14 +3,24 @@ package com.vicarius.vicarius.configuration;
 import org.apache.http.HttpHost;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ElasticConfig {
 
+    @Value("${elastic.hostname}")
+    private String hostname;
+
+    @Value("${elastic.port}")
+    private Integer port;
+
+    @Value("${elastic.scheme}")
+    private String scheme;
+
     @Bean
     public RestHighLevelClient restHighLevelClient() {
-        return new RestHighLevelClient(RestClient.builder(new HttpHost("localhost", 9200, "http")));
+        return new RestHighLevelClient(RestClient.builder(new HttpHost(hostname, port, scheme)));
     }
 }
